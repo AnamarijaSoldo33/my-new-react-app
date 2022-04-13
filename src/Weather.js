c22762c8f61df9a91ebe2d70ced4b775705e5462
AnamarijaSoldo33 <anamarija.ams3@gmail.com>
2022-04-13 20:33:13 +0200

    icon url

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       city: response.data.name,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      icon: "https://ssl.gstatic.com/onebox/weather/64/rain_light.png",
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
     });
   }
